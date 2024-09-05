ec37db899e82dd0ae9b220a949da68e10c22c2ac
DankAlighieri <guilhermeemeterioif@gmail.com>
2024-09-05 15:36:25 -0300

    1119 solution in construction

--- Beecrowd/1119.c
+++ Beecrowd/1119.c
@@ -29,35 +29,55 @@ void ins(lista *pl, int N, int tot) {
   if (!N) {
     return;
   }
+
   NODO *novo, *aux;
+
+  // alocando memoria
   novo = (NODO *)malloc(sizeof(NODO));
   if (!novo) {
     puts("Mem err");
     return;
   }
+
+  // setando posicao
   novo->pos = tot - N + 1;
+
+  // lista vazia
   if (!(*pl)) {
     novo->next = novo;
     novo->ant = novo;
-    *pl = novo;
-  } else {
-    NODO *aux=*pl;
-    
+  }
+
+  /*
+    Lista nao vazia
+    apenas um caso de insercao - ultimo elemento
+  */
+  else {
+    NODO *aux = *pl;
+
     novo->next = aux->next;
-    aux->next = novo;
     novo->ant = aux;
     aux->next->ant = novo;
-    (*pl) = novo;
+    aux->next = novo;
   }
+
+  // referencia externa sera o ultimo nodo
+  *pl = novo;
   ins(pl, N - 1, tot);
 }
 
-int recup(lista l, int k) {
-    for (; k > 0; k--, l=l->next);
-    return l->pos;
+void ret(NODO *node) {
+  node->ant->next = node->next;
+  node->next->ant = node->ant;
+  free(node);
 }
 
-void search(lista *pl, int k, int m);
+void search(lista *pl, int k, int m) {
+  NODO *nd, *ne;
+  int posN, posA;
+
+  
+}
 
 void mostra(lista l) {
   NODO *aux = l->next;
@@ -71,8 +91,21 @@ void mostra(lista l) {
 
 int main() {
   lista l;
+  int k, m, N;
+
   cria_lista(&l);
-  ins(&l, 10, 10);
-  mostra(l);
+  do {
+    scanf("%d %d %d", &N, &k, &m);
+
+    if (N && k && m) {
+      ins(&l, N, N);
+      /*
+      while (l) {
+        // search(&l, k, m);
+       };
+      */
+    }
+  } while (N);
+
   return 0;
 }
